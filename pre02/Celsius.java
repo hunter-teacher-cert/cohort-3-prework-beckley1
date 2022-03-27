40e914fade64f5034ea6dfeeea0ed9e2ca4a9fed
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-03-27 19:04:16 +0000

    fixed bug celsius.java

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 /**
- * Converts Celcius to Fahrenheit 
+ * Converts Celsius to Fahrenheit 
  */
 
 public class Celsius{
    public static void main(String args[]){
-      Scanner console = new Scanner(System.in);
+      Scanner in = new Scanner(System.in);
       System.out.print("Input temperature in Celsius: ");
-      double temperature = console.nextDouble();
+      double temperature = in.nextDouble();
       double temperatureInF = temperature * 9 / 5 + 32;
       System.out.printf("%.1f C = %.1f F", temperature, temperatureInF);
    }
