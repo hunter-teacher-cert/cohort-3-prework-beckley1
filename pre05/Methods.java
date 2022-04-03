af531b07d8637370ea8af3010c44a0abf3de9d6f
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-04-03 19:39:15 +0000

    completed Methods.java for pre05

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -27,11 +27,27 @@ public class Methods{
   }
 
   //This is the method for Ex. 8 in Think Java Chapter 6
-  public static boolean
+  public static int ack(int m, int n){
+    
+    if (m==0){
+      return n+1;
+    }
+    else if ((m>0)&&(n==0)){
+      return ack(m-1, 1);
+    }
+    else if ((m>0)&&(n>0)){
+      return ack(m-1, ack(m, n-1));
+    }
+    else {
+      return 0;
+    }
+    
+  }
 
   
   public static void main(String[] args){
     System.out.println(isDivisible(12,3));
     System.out.println(isTriangle(1,3,4));
+    System.out.println(ack(3,4));
   }
 }
