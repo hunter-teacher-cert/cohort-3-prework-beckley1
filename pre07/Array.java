67fdb7a7cf13695cfcc195d079ec0ea155aa50aa
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-04-25 00:20:14 +0000

    Did more pre07 work Assignments 1 and 2 almost done

--- pre07/Array.java
+++ pre07/Array.java
@@ -1,10 +1,23 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Array{
+   
+    public static double[] powArray(double[] a, double power){
+    double[] values = new double[a.length];
+    for (int i=0;i < a.length;i++){
+      values[i] = Math.pow(a[i],power);
+}
+      return values;
 
-  public static double[] powArray (double[] a, double power){
-    double[] raisePower
-    
+      }    
+  public static void main (String[] args){
+    System.out.println("Exercise 1a from Think Java Chapter 8.");
+    double[] a = {1.0, 2.0, 3.0, 4.0, 5.0};
+    System.out.print(powArray(a, 3.0));
   }
+
+
+
+
 }
