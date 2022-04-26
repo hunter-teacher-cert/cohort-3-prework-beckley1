60fc9ca4c2214f6f8e058426e3ed5b3880ea42dd
Benjamin Eckley <97626329+beckley1@users.noreply.github.com>
2022-04-26 15:50:25 +0000

    more debugging in exercise 1

--- pre07/Array.java
+++ pre07/Array.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Array{
-   
+   //method for exponential array values (exercise 1a)
     public static double[] powArray(double[] a, double power){
     double[] values = new double[a.length];
     for (int i=0;i < a.length;i++){
@@ -11,10 +11,23 @@ public class Array{
       return values;
 
       }    
+
+  //method for histogram (exercise 1b)
+   public static int[] histogram(int[] scores, int counters){
+      int[] counter = new int[counters];
+      for (int element:scores){
+         counter[element]++;
+      }
+      return counter;
+   }
   public static void main (String[] args){
     System.out.println("Exercise 1a from Think Java Chapter 8.");
     double[] a = {1.0, 2.0, 3.0, 4.0, 5.0};
-    System.out.print(powArray(a, 3.0));
+    //Array exponent = new Array();
+    //double[] result;
+   // result = exponent.powArray(a, 3.0);
+    System.out.print("Exponential is " + result);
+    //Can't get this to print!!!!
   }
 
 
